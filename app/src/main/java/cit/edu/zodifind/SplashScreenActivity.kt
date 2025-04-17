e3de493a552e62dbbe665e9ae51dbdfaa03223a2
Aaron <aaronvjatayna@gmail.com>
2025-04-18 02:53:55 +0800

    splashscreen complete

--- app/src/main/java/cit/edu/zodifind/SplashScreenActivity.kt
+++ app/src/main/java/cit/edu/zodifind/SplashScreenActivity.kt
@@ -7,63 +7,39 @@ import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.view.SurfaceHolder
-import android.view.SurfaceView
-import android.widget.RelativeLayout
+import android.widget.VideoView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : Activity() {
 
-    private lateinit var surfaceView: SurfaceView
+    private lateinit var videoView: VideoView
     private lateinit var mediaPlayer: MediaPlayer
+    @SuppressLint("MissingInflatedId")
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splashscreen)
 
-        surfaceView = findViewById(R.id.svGalaxy)
-        playBackgroundVideo()
+        playVideoView()
 
-        val rlSplashScreenLayout = findViewById<RelativeLayout>(R.id.rlSplashScreenLayout)
+        val constraintLayoutSplashScreen = findViewById<ConstraintLayout>(R.id.constraintLayoutSplashScreen)
 
-        rlSplashScreenLayout.setOnClickListener(){
+        constraintLayoutSplashScreen.setOnClickListener(){
             val intent = Intent(this, LoginActivity:: class.java)
             startActivity(intent)
             finish()
         }
     }
-    private fun playBackgroundVideo() {
-        val holder: SurfaceHolder = surfaceView.holder
-
-        mediaPlayer = MediaPlayer()
-
-        // Set up the SurfaceHolder callback
-        holder.addCallback(object : SurfaceHolder.Callback {
-            override fun surfaceCreated(holder: SurfaceHolder) {
-                // Set up MediaPlayer when surface is created
-                val videoPath = "android.resource://${packageName}/${R.raw.galaxy}"
-                val uri: Uri = Uri.parse(videoPath)
-
-                mediaPlayer.setDataSource(this@SplashScreenActivity, uri)
-                mediaPlayer.setDisplay(holder)  // Set the display to SurfaceView
-                mediaPlayer.isLooping = true  // Enable looping
-
-                mediaPlayer.prepareAsync()
-
-                mediaPlayer.setOnPreparedListener {
-                    mediaPlayer.start()  // Start the video
-                }
-            }
-
-            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-                // Optional: handle surface size changes if needed
-            }
-
-            override fun surfaceDestroyed(holder: SurfaceHolder) {
-                mediaPlayer.release()  // Release the MediaPlayer when the surface is destroyed
-            }
-        })
+    private fun playVideoView() {
+        videoView = findViewById(R.id.vvGalaxy)
+        val videoUri = Uri.parse("android.resource://${packageName}/${R.raw.splashscreen_video}")
+        videoView.setVideoURI(videoUri)
+        videoView.setOnCompletionListener {
+            videoView.start()
+        }
+        videoView.start()
     }
-
 
 }
 
