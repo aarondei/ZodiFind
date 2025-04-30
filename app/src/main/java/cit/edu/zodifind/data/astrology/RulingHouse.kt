0ee2980d411ef0accc0650c00b0cdb6d4778f429
Aaron <aaronvjatayna@gmail.com>
2025-05-01 02:40:32 +0800

    data

---
+++ app/src/main/java/cit/edu/zodifind/data/astrology/RulingHouse.kt
@@ -0,0 +1,18 @@
+package cit.edu.zodifind.data.astrology
+
+import cit.edu.zodifind.R
+
+enum class RulingHouse(val symbolIcon: Int, val description: Int) {
+    FIRST(R.drawable.star, R.string.rulinghouse),
+    SECOND(R.drawable.star, R.string.rulinghouse),
+    THIRD(R.drawable.star, R.string.rulinghouse),
+    FOURTH(R.drawable.star, R.string.rulinghouse),
+    FIFTH(R.drawable.star, R.string.rulinghouse),
+    SIXTH(R.drawable.star, R.string.rulinghouse),
+    SEVENTH(R.drawable.star, R.string.rulinghouse),
+    EIGHTH(R.drawable.star, R.string.rulinghouse),
+    NINTH(R.drawable.star, R.string.rulinghouse),
+    TENTH(R.drawable.star, R.string.rulinghouse),
+    ELEVENTH(R.drawable.star, R.string.rulinghouse),
+    TWELFTH(R.drawable.star, R.string.rulinghouse)
+}
--- app/src/main/java/cit/edu/zodifind/data/astrology/RulingPlanet.kt
+++ app/src/main/java/cit/edu/zodifind/data/astrology/RulingPlanet.kt
@@ -1,4 +1,18 @@
 package cit.edu.zodifind.data.astrology
 
+import cit.edu.zodifind.R
+
 enum class RulingPlanet(val symbolIcon: Int, val description: Int) {
+    MERCURY_GEMINI(R.drawable.star, R.string.rulingplanet_mercury_gemini),
+    MERCURY_VIRGO(R.drawable.star, R.string.rulingplanet_mercury_virgo),
+    VENUS_TAURUS(R.drawable.star, R.string.rulingplanet_venus_taurus),
+    VENUS_LIBRA(R.drawable.star, R.string.rulingplanet_venus_libra),
+    MARS(R.drawable.star, R.string.rulingplanet_mars),
+    JUPITER(R.drawable.star, R.string.rulingplanet_jupiter),
+    SATURN(R.drawable.star, R.string.rulingplanet_saturn),
+    URANUS(R.drawable.star, R.string.rulingplanet_uranus),
+    NEPTUNE(R.drawable.star, R.string.rulingplanet_neptune),
+    PLUTO(R.drawable.star, R.string.rulingplanet_pluto),
+    SUN(R.drawable.star, R.string.rulingplanet_sun),
+    MOON(R.drawable.star, R.string.rulingplanet_moon)
 }
