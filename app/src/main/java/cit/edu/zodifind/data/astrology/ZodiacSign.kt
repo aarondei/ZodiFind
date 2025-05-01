5ff50bdff97d2c152bcfdc8f7964f9d8839dd872
Aaron <aaronvjatayna@gmail.com>
2025-05-01 13:33:00 +0800

    changes

--- app/src/main/java/cit/edu/zodifind/data/astrology/ZodiacSign.kt
+++ app/src/main/java/cit/edu/zodifind/data/astrology/ZodiacSign.kt
@@ -7,18 +7,18 @@ import java.time.LocalDate
 
 @Suppress("DEPRECATION")
 enum class ZodiacSign(val faceIcon: Int, val starIcon: Int, val representation: String, val startDate: String, val endDate: String, val sketchIcon: Int, val symbolIcon: Int, val element: Element, val quality: Quality, val rulingPlanet: RulingPlanet, val rulingHouse: RulingHouse, val compatibility: String) {
-    ARIES(R.drawable.aries_icon, R.drawable.aries_cons, "The Ram","MAR 21", "APR 19", R.drawable.star, R.drawable.symbol_aries, Element.FIRE, Quality.CARDINAL, RulingPlanet.MARS, RulingHouse.FIRST, "LEO"),
-    TAURUS(R.drawable.taurus_icon, R.drawable.taurus_cons,"The Bull","APR 20", "MAY 20",  R.drawable.star, R.drawable.symbol_taurus, Element.EARTH, Quality.FIXED, RulingPlanet.VENUS_TAURUS, RulingHouse.SECOND, "CAPRICORN"),
-    GEMINI(R.drawable.gemini_icon, R.drawable.gemini_cons,"The Twins","MAY 21", "JUN 20",  R.drawable.star, R.drawable.symbol_gemini, Element.AIR, Quality.MUTABLE, RulingPlanet.MERCURY_GEMINI, RulingHouse.THIRD, "LIBRA"),
-    CANCER(R.drawable.cancer_icon, R.drawable.cancer_cons,"The Crab","JUN 21", "JUL 22",  R.drawable.star, R.drawable.symbol_cancer, Element.WATER, Quality.CARDINAL, RulingPlanet.MOON, RulingHouse.FOURTH, "SCORPIO"),
-    LEO(R.drawable.leo_icon, R.drawable.leo_cons,"The Lion","JUL 23", "AUG 22",  R.drawable.star, R.drawable.symbol_leo, Element.FIRE, Quality.FIXED, RulingPlanet.SUN, RulingHouse.FIFTH, "SAGITTARIUS"),
-    VIRGO(R.drawable.virgo_icon, R.drawable.virgo_cons,"The Maiden","AUG 23", "SEPT 22",  R.drawable.star, R.drawable.symbol_virgo, Element.EARTH, Quality.MUTABLE, RulingPlanet.MERCURY_VIRGO, RulingHouse.SIXTH,"TAURUS" ),
-    LIBRA(R.drawable.libra_icon, R.drawable.libra_cons,"The Scales","SEPT 23","OCT 22",  R.drawable.star, R.drawable.symbol_libra, Element.AIR, Quality.CARDINAL, RulingPlanet.VENUS_LIBRA, RulingHouse.SEVENTH, "AQUARIUS"),
-    SCORPIO(R.drawable.scorpio_icon, R.drawable.scorpio_cons,"The Scorpion","OCT 23", "NOV 21",  R.drawable.star, R.drawable.symbol_scorpio, Element.WATER, Quality.FIXED, RulingPlanet.PLUTO, RulingHouse.EIGHTH, "PISCES"),
-    SAGITTARIUS(R.drawable.sagittarius_icon, R.drawable.sagittarius_cons,"The Archer","NOV 22", "DEC 21",  R.drawable.star, R.drawable.symbol_sagittarius, Element.FIRE, Quality.MUTABLE, RulingPlanet.JUPITER, RulingHouse.NINTH, "ARIES"),
-    CAPRICORN(R.drawable.capricorn_icon, R.drawable.capricorn_cons,"The Goat","DEC 22", "JAN 19",  R.drawable.star, R.drawable.symbol_capricorn, Element.EARTH, Quality.CARDINAL, RulingPlanet.SATURN, RulingHouse.TENTH, "TAURUS"),
-    AQUARIUS(R.drawable.aquarius_icon, R.drawable.aquarius_cons,"The Water Bearer","JAN 20", "FEB 18",  R.drawable.star, R.drawable.symbol_aquarius, Element.AIR, Quality.FIXED, RulingPlanet.URANUS, RulingHouse.ELEVENTH, "GEMINI"),
-    PISCES(R.drawable.pisces_icon, R.drawable.pisces_cons,"The Fish","FEB 19", "MAR 20",  R.drawable.star, R.drawable.symbol_pisces, Element.WATER, Quality.MUTABLE, RulingPlanet.NEPTUNE, RulingHouse.TWELFTH, "CANCER");
+    ARIES(R.drawable.aries_icon, R.drawable.aries_cons, "The Ram","MAR 21", "APR 19", R.drawable.sketch_aries, R.drawable.symbol_aries, Element.FIRE, Quality.CARDINAL, RulingPlanet.MARS, RulingHouse.FIRST, "LEO"),
+    TAURUS(R.drawable.taurus_icon, R.drawable.taurus_cons,"The Bull","APR 20", "MAY 20",  R.drawable.sketch_taurus, R.drawable.symbol_taurus, Element.EARTH, Quality.FIXED, RulingPlanet.VENUS_TAURUS, RulingHouse.SECOND, "CAPRICORN"),
+    GEMINI(R.drawable.gemini_icon, R.drawable.gemini_cons,"The Twins","MAY 21", "JUN 20",  R.drawable.sketch_gemini, R.drawable.symbol_gemini, Element.AIR, Quality.MUTABLE, RulingPlanet.MERCURY_GEMINI, RulingHouse.THIRD, "LIBRA"),
+    CANCER(R.drawable.cancer_icon, R.drawable.cancer_cons,"The Crab","JUN 21", "JUL 22",  R.drawable.sketch_cancer, R.drawable.symbol_cancer, Element.WATER, Quality.CARDINAL, RulingPlanet.MOON, RulingHouse.FOURTH, "SCORPIO"),
+    LEO(R.drawable.leo_icon, R.drawable.leo_cons,"The Lion","JUL 23", "AUG 22",  R.drawable.sketch_leo, R.drawable.symbol_leo, Element.FIRE, Quality.FIXED, RulingPlanet.SUN, RulingHouse.FIFTH, "SAGITTARIUS"),
+    VIRGO(R.drawable.virgo_icon, R.drawable.virgo_cons,"The Maiden","AUG 23", "SEPT 22",  R.drawable.sketch_virgo, R.drawable.symbol_virgo, Element.EARTH, Quality.MUTABLE, RulingPlanet.MERCURY_VIRGO, RulingHouse.SIXTH,"TAURUS" ),
+    LIBRA(R.drawable.libra_icon, R.drawable.libra_cons,"The Scales","SEPT 23","OCT 22",  R.drawable.sketch_libra, R.drawable.symbol_libra, Element.AIR, Quality.CARDINAL, RulingPlanet.VENUS_LIBRA, RulingHouse.SEVENTH, "AQUARIUS"),
+    SCORPIO(R.drawable.scorpio_icon, R.drawable.scorpio_cons,"The Scorpion","OCT 23", "NOV 21",  R.drawable.sketch_scorpio, R.drawable.symbol_scorpio, Element.WATER, Quality.FIXED, RulingPlanet.PLUTO, RulingHouse.EIGHTH, "PISCES"),
+    SAGITTARIUS(R.drawable.sagittarius_icon, R.drawable.sagittarius_cons,"The Archer","NOV 22", "DEC 21",  R.drawable.sketch_sagittarius, R.drawable.symbol_sagittarius, Element.FIRE, Quality.MUTABLE, RulingPlanet.JUPITER, RulingHouse.NINTH, "ARIES"),
+    CAPRICORN(R.drawable.capricorn_icon, R.drawable.capricorn_cons,"The Goat","DEC 22", "JAN 19",  R.drawable.sketch_capricorn, R.drawable.symbol_capricorn, Element.EARTH, Quality.CARDINAL, RulingPlanet.SATURN, RulingHouse.TENTH, "TAURUS"),
+    AQUARIUS(R.drawable.aquarius_icon, R.drawable.aquarius_cons,"The Water Bearer","JAN 20", "FEB 18",  R.drawable.sketch_aquarius, R.drawable.symbol_aquarius, Element.AIR, Quality.FIXED, RulingPlanet.URANUS, RulingHouse.ELEVENTH, "GEMINI"),
+    PISCES(R.drawable.pisces_icon, R.drawable.pisces_cons,"The Fish","FEB 19", "MAR 20",  R.drawable.sketch_pisces, R.drawable.symbol_pisces, Element.WATER, Quality.MUTABLE, RulingPlanet.NEPTUNE, RulingHouse.TWELFTH, "CANCER");
 
     fun concatStartEndDates(): String {
         return "$startDate - $endDate"
--- app/src/main/java/cit/edu/zodifind/data/palmistry/PalmLines.kt
+++ app/src/main/java/cit/edu/zodifind/data/palmistry/PalmLines.kt
@@ -6,14 +6,14 @@ import cit.edu.zodifind.luck.PalmModel
 class PalmLines () {
 
     private val allLines = listOf(
-        PalmModel(R.drawable.star, R.string.girdleofvenus_title , R.string.girdleofvenus_domain , R.string.girdleofvenus_content),
-        PalmModel(R.drawable.star, R.string.marriageline_title , R.string.marriageline_domain , R.string.marriageline_content),
-        PalmModel(R.drawable.star, R.string.lineofintuition_title , R.string.lineofintuition_domain , R.string.lineofintuition_content),
-        PalmModel(R.drawable.star, R.string.lineofsun_title , R.string.lineofsun_domain , R.string.lineofsun_content),
-        PalmModel(R.drawable.star, R.string.lineofdestiny_title , R.string.lineofdestiny_domain , R.string.lineofdestiny_content),
-        PalmModel(R.drawable.star, R.string.lineofhealth_title , R.string.lineofhealth_domain , R.string.lineofhealth_content),
-        PalmModel(R.drawable.star, R.string.lineofheart_title , R.string.lineofheart_domain , R.string.lineofheart_content),
-        PalmModel(R.drawable.star, R.string.lineofhead_title , R.string.lineofhead_domain , R.string.lineofhead_content),
-        PalmModel(R.drawable.star, R.string.lineofmars_title, R.string.lineofmars_domain , R.string.lineofmars_content),
+        PalmModel(R.drawable.girdleofvenus_domain, R.string.girdleofvenus_title , R.string.girdleofvenus_domain , R.string.girdleofvenus_content),
+        PalmModel(R.drawable.marriageline_domain, R.string.marriageline_title , R.string.marriageline_domain , R.string.marriageline_content),
+        PalmModel(R.drawable.lineofintuition_domain, R.string.lineofintuition_title , R.string.lineofintuition_domain , R.string.lineofintuition_content),
+        PalmModel(R.drawable.lineofsun_domain, R.string.lineofsun_title , R.string.lineofsun_domain , R.string.lineofsun_content),
+        PalmModel(R.drawable.lineofdestiny_domain, R.string.lineofdestiny_title , R.string.lineofdestiny_domain , R.string.lineofdestiny_content),
+        PalmModel(R.drawable.lineofhealth_domain, R.string.lineofhealth_title , R.string.lineofhealth_domain , R.string.lineofhealth_content),
+        PalmModel(R.drawable.lineofheartdomain, R.string.lineofheart_title , R.string.lineofheart_domain , R.string.lineofheart_content),
+        PalmModel(R.drawable.lineofhead_domain, R.string.lineofhead_title , R.string.lineofhead_domain , R.string.lineofhead_content),
+        PalmModel(R.drawable.lineofmars_domain, R.string.lineofmars_title, R.string.lineofmars_domain , R.string.lineofmars_content),
     )
 }
