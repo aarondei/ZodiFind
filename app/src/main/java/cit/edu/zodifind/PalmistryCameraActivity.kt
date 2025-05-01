caf82589a7060aa3dfdd171b27f32689609e7452
Aaron <aaronvjatayna@gmail.com>
2025-05-02 00:26:07 +0800

    camera

--- app/src/main/java/cit/edu/zodifind/PalmistryCameraActivity.kt
+++ app/src/main/java/cit/edu/zodifind/PalmistryCameraActivity.kt
@@ -1,6 +1,8 @@
 package cit.edu.zodifind
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,10 @@ class PalmistryCameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.palmistrycam)
+
+        val btnBack = findViewById<ImageView>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            startActivity(Intent(this, PalmistryLibraryActivity::class.java))
+        }
     }
 }
