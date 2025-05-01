07bac9d90356721bde9039ca3e8f5bc27bd6bd57
Aaron <aaronvjatayna@gmail.com>
2025-05-02 01:19:43 +0800

    tarot reading done

--- app/src/main/java/cit/edu/zodifind/TarotHomeActivity.kt
+++ app/src/main/java/cit/edu/zodifind/TarotHomeActivity.kt
@@ -1,20 +1,24 @@
 package cit.edu.zodifind
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class TarotHomeActivity : AppCompatActivity() {
+
+    private var selectedItem: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tarot_home)
 
-        val items = listOf("Option 1", "Option 2", "Option 3")
+        val items = listOf("Love", "Career", "Money", "Personal Growth", "Health", "Life Purpose", "Spiritual Guidance", "Decision", "Family", "Friendship")
         val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, items)
         val dropdown = findViewById<AutoCompleteTextView>(R.id.dropdown)
         dropdown.setAdapter(adapter)
@@ -22,10 +26,23 @@ class TarotHomeActivity : AppCompatActivity() {
         dropdown.setOnClickListener {
             dropdown.showDropDown()
         }
+        dropdown.dropDownHeight = 1100
+
+
+        dropdown.setOnItemClickListener { parent, view, position, id ->
+            selectedItem = parent.getItemAtPosition(position).toString()
+        }
+
 
         val btnReveal = findViewById<Button>(R.id.btnReveal)
         btnReveal.setOnClickListener {
-            startActivity() // TODO INFLATE FRAGMENT DECK
+            if (selectedItem != null) {
+                val intent = Intent(this, TarotDeckActivity::class.java)
+                intent.putExtra("reading", selectedItem)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Please select a reading first", Toast.LENGTH_LONG).show()
+            }
         }
 
 
