c5e9723476cc1830bc442e27d76a8f2503bedf33
vncejnh <venicejuarez0809@gmail.com>
2025-05-02 00:16:01 +0800

    implemented palmistry (though i think need smaller ang camera button hehe)

---
+++ app/src/main/java/cit/edu/zodifind/PalmistryCameraActivity.kt
@@ -0,0 +1,14 @@
+package cit.edu.zodifind
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PalmistryCameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.palmistrycam)
+    }
+}
--- app/src/main/java/cit/edu/zodifind/data/palmistry/PalmLines.kt
+++ app/src/main/java/cit/edu/zodifind/data/palmistry/PalmLines.kt
@@ -3,17 +3,20 @@ package cit.edu.zodifind.data.palmistry
 import cit.edu.zodifind.R
 import cit.edu.zodifind.luck.PalmModel
 
-class PalmLines () {
-
-    private val allLines = listOf(
-        PalmModel(R.drawable.girdleofvenus_domain, R.string.girdleofvenus_title , R.string.girdleofvenus_domain , R.string.girdleofvenus_content),
-        PalmModel(R.drawable.marriageline_domain, R.string.marriageline_title , R.string.marriageline_domain , R.string.marriageline_content),
-        PalmModel(R.drawable.lineofintuition_domain, R.string.lineofintuition_title , R.string.lineofintuition_domain , R.string.lineofintuition_content),
-        PalmModel(R.drawable.lineofsun_domain, R.string.lineofsun_title , R.string.lineofsun_domain , R.string.lineofsun_content),
-        PalmModel(R.drawable.lineofdestiny_domain, R.string.lineofdestiny_title , R.string.lineofdestiny_domain , R.string.lineofdestiny_content),
-        PalmModel(R.drawable.lineofhealth_domain, R.string.lineofhealth_title , R.string.lineofhealth_domain , R.string.lineofhealth_content),
-        PalmModel(R.drawable.lineofheartdomain, R.string.lineofheart_title , R.string.lineofheart_domain , R.string.lineofheart_content),
-        PalmModel(R.drawable.lineofhead_domain, R.string.lineofhead_title , R.string.lineofhead_domain , R.string.lineofhead_content),
-        PalmModel(R.drawable.lineofmars_domain, R.string.lineofmars_title, R.string.lineofmars_domain , R.string.lineofmars_content),
-    )
+class PalmLines {
+    companion object {
+        fun getAllPalmLines(): List<PalmModel> {
+            return listOf(
+                PalmModel(R.drawable.girdleofvenus_domain, R.string.girdleofvenus_title, R.string.girdleofvenus_domain, R.string.girdleofvenus_content),
+                PalmModel(R.drawable.marriageline_domain, R.string.marriageline_title, R.string.marriageline_domain, R.string.marriageline_content),
+                PalmModel(R.drawable.lineofintuition_domain, R.string.lineofintuition_title, R.string.lineofintuition_domain, R.string.lineofintuition_content),
+                PalmModel(R.drawable.lineofsun_domain, R.string.lineofsun_title, R.string.lineofsun_domain, R.string.lineofsun_content),
+                PalmModel(R.drawable.lineofdestiny_domain, R.string.lineofdestiny_title, R.string.lineofdestiny_domain, R.string.lineofdestiny_content),
+                PalmModel(R.drawable.lineofhealth_domain, R.string.lineofhealth_title, R.string.lineofhealth_domain, R.string.lineofhealth_content),
+                PalmModel(R.drawable.lineofheartdomain, R.string.lineofheart_title, R.string.lineofheart_domain, R.string.lineofheart_content),
+                PalmModel(R.drawable.lineofhead_domain, R.string.lineofhead_title, R.string.lineofhead_domain, R.string.lineofhead_content),
+                PalmModel(R.drawable.lineofmars_domain, R.string.lineofmars_title, R.string.lineofmars_domain, R.string.lineofmars_content)
+            )
+        }
+    }
 }
