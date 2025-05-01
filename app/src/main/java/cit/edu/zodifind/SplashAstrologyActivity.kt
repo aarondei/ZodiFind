3a77c4bea8f369da72991a4fc9a0b9c6f9f522e1
Aaron <aaronvjatayna@gmail.com>
2025-05-01 13:17:26 +0800

    splash features done

---
+++ app/src/main/java/cit/edu/zodifind/SplashAstrologyActivity.kt
@@ -0,0 +1,22 @@
+package cit.edu.zodifind
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class SplashAstrologyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_astrology)
+
+
+        val splashscreen = findViewById<ConstraintLayout>(R.id.splash_astrology)
+        splashscreen.setOnClickListener(){
+            val intent = Intent(this, SplashTarotActivity:: class.java) //TODO CHANGE
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
---
+++ app/src/main/java/cit/edu/zodifind/SplashPalmistryActivity.kt
@@ -0,0 +1,23 @@
+package cit.edu.zodifind
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashPalmistryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_palmistry)
+
+        val splashscreen = findViewById<ConstraintLayout>(R.id.splash_palmistry)
+        splashscreen.setOnClickListener(){
+            val intent = Intent(this, HomeActivity:: class.java) //TODO CHANGE
+            startActivity(intent)
+            finish()
+        }
+    }
+}
